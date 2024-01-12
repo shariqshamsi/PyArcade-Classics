ebadfc4d22f9178d5e1b1178901503631d376b18
shariqshamsi <shariqtanzeem@hotmail.com>
2024-01-12 12:11:02 +0500

    Python-based Mad Libs game generating stories by prompting user input to fill in story blanks
    Experience a creative and entertaining word game where users input words to fill in blanks within a predefined story template. This interactive program then generates a unique and often humorous narrative by incorporating the user's provided words. Built in Python for a fun and dynamic language manipulation experience

---
+++ madlibs/madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+
+madlib = f"computer programing is so {adj}! It makes me so excited all the time becuase \
+I love to {verb1}. Stay hyderated and {verb2} like you are {famous_person}!" 
+
+print(madlib)
